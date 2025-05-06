518caa856762b0c57eeb8b163d45db586adf7c6d
Simn-popo <paulin.simon@live.fr>
2025-05-06 14:10:12 +0200

    OG

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         char = ord(str[i])
-        if char > 96 and char <= 122:
+        if char > 96 and char < 123:
             char = char - 32
         print({}.format(chr(char)), end="")
     print()
