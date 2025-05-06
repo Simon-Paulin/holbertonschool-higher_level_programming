1cb21a38dd42ddd0333d13a88edbb28e6d67e1ee
Simn-popo <paulin.simon@live.fr>
2025-05-06 14:13:46 +0200

    add: "" to {}

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         char = ord(str[i])
         if char > 96 and char < 123:
             char = char - 32
-        print({}.format(chr(char)), end="")
+        print("{}".format(chr(char)), end="")
     print()
