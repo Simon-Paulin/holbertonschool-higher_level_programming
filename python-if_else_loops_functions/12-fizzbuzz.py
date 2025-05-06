86762197faf44f92fc358739c1567862cc3cb1ad
Simn-popo <paulin.simon@live.fr>
2025-05-06 15:35:47 +0200

    add: for insted if

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    i = 0
-    if i in range(1, 101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz ", end="")
         elif i % 3 == 0:
