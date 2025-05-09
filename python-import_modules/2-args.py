d676d9adf6c3d00d524591f72e32cccf9362b2af
Simn-popo <paulin.simon@live.fr>
2025-05-09 12:39:50 +0200

    add: : if i got more that 1 arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("{} argument:".format(len(argv) - 1))
         print("1: {}".format(argv[1]))
     elif len(argv) > 2:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
     else:
