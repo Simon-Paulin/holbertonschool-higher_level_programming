49241456bf1502e09c6cca93e006939032590519
Simn-popo <paulin.simon@live.fr>
2025-05-13 18:21:36 +0200

    add:file

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
+
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
