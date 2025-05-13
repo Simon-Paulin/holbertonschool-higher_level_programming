0d202a141d0ffab00b1ec518300582756fb483e8
Simn-popo <paulin.simon@live.fr>
2025-05-13 18:24:02 +0200

    fix: rm local test file

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,9 +4,3 @@ def multiple_returns(sentence):
         return (0, None)
     else:
         return (len(sentence), sentence[0])
-
-multiple_returns = __import__('8-multiple_returns').multiple_returns
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
