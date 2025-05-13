a85cd131f8634832ccd40715f25401454726eb72
Simn-popo <paulin.simon@live.fr>
2025-05-13 13:18:58 +0200

    fix :indentation + take out test script

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,15 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(line[i]), end=" ")
             else:
                 print("{:d}".format(line[i]), end="")
-            print()
-
-if __name__ == "__main__":
-    matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+        print()
