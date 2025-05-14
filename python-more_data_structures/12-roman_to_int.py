01cdb1bba3ed5f5acce87d6e4b27d4e6f233a9f4
Simn-popo <paulin.simon@live.fr>
2025-05-14 14:57:56 +0200

    fix peco

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string) or roman_string is None:
-    if not isinstance(roman_string, str):
-        return (None)
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
     roman_value = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
@@ -14,8 +14,7 @@ def roman_to_int(roman_string) or roman_string is None:
 
         if value < prev_result:
             result -= value
-        else: 
+        else:
             result += value
-        
         prev_result = value
     return (result)
