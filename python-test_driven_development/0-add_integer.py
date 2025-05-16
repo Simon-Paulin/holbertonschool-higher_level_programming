53482cb61249ed583dee91aeed19f05c6aa0b469
Simn-popo <paulin.simon@live.fr>
2025-05-16 15:18:03 +0200

    add:file py txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """
-    that add integer
-
+    integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
