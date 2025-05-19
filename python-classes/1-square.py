c9cf4c437d895e6a15eb1698a77a77a9898465eb
Simn-popo <paulin.simon@live.fr>
2025-05-19 10:50:36 +0200

    add:file

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+create a new class named "Square"
+'''
+
+
+class Square:
+    '''
+    class Square
+    '''
+
+
+def __init__(self, size):
+    '''
+    initialised a new square whit size
+    '''
