3a1959040062c4d204ecf9a9cf2a402188cc6e19
Simn-popo <paulin.simon@live.fr>
2025-05-20 11:52:00 +0200

    fix: perpission

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,8 +21,8 @@ class Rectangle:
         TypeError: if widht or height is not a integer
         ValueError: if widht or height is < 0
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
