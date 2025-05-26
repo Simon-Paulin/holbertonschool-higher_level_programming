fc26ffb5cc9ce3f27788a6dffc18d2ce7c00ea46
Simn-popo <paulin.simon@live.fr>
2025-05-26 11:43:00 +0200

    add:descriptiton

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 '''
+1-my_list.py
 Write a class MyList that inherits from list
 '''
 
 
 class MyList(list):
+    '''
+    Class MyList
+    '''
+
     def print_sorted(self):
         print(sorted(self))
