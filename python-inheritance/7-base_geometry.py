ce0504f3f70728a192b9657322bfec3c7694cdbe
Simn-popo <paulin.simon@live.fr>
2025-05-26 17:54:59 +0200

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+7-base_geometry.py
+Integer validator
+'''
+
+
+class BaseGeometry:
+    '''
+    Class created
+    '''
+
+    def area(self):
+        '''
+        that raises an Exception
+        '''
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
