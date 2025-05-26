ac6d2d3c97e1e986bbbea9f8d48388b15db7c5f6
Simn-popo <paulin.simon@live.fr>
2025-05-26 17:01:13 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+6-base_geometry.py
+Write a class BaseGeometry (based on 5-base_geometry.py)
+'''
+
+
+class BaseGeometry:
+    '''
+    Class created
+    '''
+
+    def area(self):
+        '''
+        that raises an Exception
+        '''
+
+        raise Exception("area() is not implemented")
