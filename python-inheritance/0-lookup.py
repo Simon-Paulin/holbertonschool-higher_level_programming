a1894a3040703581258b305e7529bece2bb98faa
Simn-popo <paulin.simon@live.fr>
2025-05-26 11:39:33 +0200

    add : description

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 '''
+0-lookup.py
 Write a function that returns the list
 of available attributes and methods of an object
 '''
 
 
 def lookup(obj):
+    '''
+    return the list
+    '''
+    
     return dir(obj)
