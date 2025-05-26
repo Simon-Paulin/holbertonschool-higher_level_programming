2651325b1022e17f7d156128c9e4054c38f027d5
Simn-popo <paulin.simon@live.fr>
2025-05-26 19:39:07 +0200

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+11-square.py
+Write a class Square that inherits from Rectangle
+'''
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    '''
+    Created new Square class
+    '''
+
+    def __init__(self, size):
+        '''
+        Initialized a square whit side size whit private attribute
+        '''
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''
+        Calculate the area of the Square
+        '''
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        '''
+        return value of square
+        '''
+
+        return ("[Square] {}/{}".format(self.__size, self.__size))
