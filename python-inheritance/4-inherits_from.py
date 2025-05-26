9e929252258b910ab2e1d2934996820df9219ef4
Simn-popo <paulin.simon@live.fr>
2025-05-26 16:38:51 +0200

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+4-inherits_from.py
+Write a function that returns
+    True: if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+
+    False: if not
+'''
+
+
+def inherits_from(obj, a_class):
+    if isinstance(obj, a_class):
+        if type(obj) is not a_class:
+            return (True)
+        else:
+            return (False)
