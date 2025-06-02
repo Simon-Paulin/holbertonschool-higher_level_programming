1d622edbeb1c69805df7eecc84cc09fc9fe2b096
Simn-popo <paulin.simon@live.fr>
2025-06-02 15:46:10 +0200

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+6-load_from_json_file.py
+'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    Write a function that creates an Object from a “JSON file”
+    '''
+
+    with open(filename, "r") as file:
+        json.load(file)
