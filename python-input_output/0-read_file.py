cf6bda70eae94ac43b95a15f5ba780380d030c0f
Simn-popo <paulin.simon@live.fr>
2025-06-02 11:05:38 +0200

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+0-read_file.py
+Write a function that reads a text file
+(UTF8) and prints it to stdout.
+'''
+
+def read_file(filename=""):
+    with open(filename, "r") as file:
+        content = file.read()
+        print(content)
