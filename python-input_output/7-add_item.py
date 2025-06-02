d1cba6996b349d4be9af96e574b888f5915732d1
Simn-popo <paulin.simon@live.fr>
2025-06-02 16:23:13 +0200

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+7-add_item.py
+'''
+
+
+import json
+import os
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    try:
+        my_list = load_from_json_file(filename)
+    except Exception:
+        my_list = []
+else:
+    my_list = []
+
+new_items = sys.argv[1:]
+for item in new_items:
+    my_list.append(item)
+
+save_to_json_file(my_list, filename)
