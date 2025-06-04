e6e4d5edba5a37ea575f82cc8b9be31032e97f06
Simn-popo <paulin.simon@live.fr>
2025-06-04 11:49:54 +0200

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+10-students.py
+'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, str) and all(isinstance(attr, str) for attr in attrs):
+                filtered_dict = {}
+                for attr in attrs:
+                    if hasattr(self, attr):
+                        filtered_dict[attr] = (self, attr)
+                    return (filtered_dict)
+        return (self.__dict__)
