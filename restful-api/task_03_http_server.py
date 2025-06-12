169a3aa2f9ef24877ad35ba18e9eb9303b93be36
Simn-popo <paulin.simon@live.fr>
2025-06-12 19:53:42 +0200

    add: dict with json et 404

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
 
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 class Api_Get(BaseHTTPRequestHandler):
     def do_GET(self):
+        call = {
+            "/": self.root,
+            "/data": self.data,
+            "/status": self.status
+        }
+        handle = call.get(self.path)
+        handle()
+
+    def root(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-
         self.wfile.write(b"Hello, this is a simple API!")
+
+    def data(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        data_js = {"name": "John", "age": 30, "city": "New York"}
+        self.wfile.write(json.dumps(data_js))
+    
+    def status(self):
+        self.send_response(404)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Status 404")
 
 host = "localhost"
 port = 8000
