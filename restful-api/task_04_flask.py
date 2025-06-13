7a9dd7c5cccb17832b6a2968e1020c458e8892a2
Simn-popo <paulin.simon@live.fr>
2025-06-13 12:03:47 +0200

    add return

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -28,15 +28,22 @@ def users_list(username):
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
-    username = data.get("username")
+
     if not data or "username" not in data:
         return jsonify({"error": "Username is required"}), 400
+
+    username = data.get("username")
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 400
+
 
     users[username] = {
         "name": data.get("name"),
         "age": data.get("age"),
         "city": data.get("city")
     }
+
     return jsonify(users[username]), 201
 
 if __name__ == "__main__":
