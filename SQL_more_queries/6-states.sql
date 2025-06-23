2ff9bc81a65e21af71ca862269991d1b6932ac73
Simn-popo <paulin.simon@live.fr>
2025-06-23 16:38:38 +0200

    try chatgps answer

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 --Create DB  and add table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    id INT NOT NULL AUTO_INCREMENT,
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256),
-    PRIMARY KEY(id),
-    UNIQUE (id)
 );
