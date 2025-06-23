216d8a1ad9cd25defb211d627250613e979db14f
Simn-popo <paulin.simon@live.fr>
2025-06-23 15:38:48 +0200

    fix IDENTIFIED

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
--- create user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'passport';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED WITH authentication_plugin BY 'passport';
 -- list all privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
