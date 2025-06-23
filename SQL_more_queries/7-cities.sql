9baf786d0c5bf2464a1ff04d8caf622991bfa451
Simn-popo <paulin.simon@live.fr>
2025-06-23 16:32:23 +0200

    7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- more
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    states_id INT NOT NULL,
+    name VARCHAR(256),
+    FOREIGN KEY (states_id) REFERENCES states(id)
+);
