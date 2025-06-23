0bd1c7b88e6d20911f3bec5ffa5a63a8ae92eb95
Simn-popo <paulin.simon@live.fr>
2025-06-23 15:58:21 +0200

    1-create_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create data base and user
-CREATE USER IF NOT EXISTS'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
