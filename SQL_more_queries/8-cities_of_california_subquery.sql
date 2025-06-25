9c7e2c9874fa5454a4b0a27440fb5464b20228db
Simn-popo <paulin.simon@live.fr>
2025-06-25 10:57:20 +0200

    8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- List all the cities
-SELECT id, name FROM cities,
+SELECT id, name FROM cities
 WHERE state_id = (
     SELECT id
     FROM states
-    WHERE name = 'California'
+    WHERE name = "California"
 )
 ORDER BY id ASC;
