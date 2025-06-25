3be5c3a12835c9876b824f8fe902e89337e764ce
Simn-popo <paulin.simon@live.fr>
2025-06-25 11:23:55 +0200

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- List all cities
-SELECT cities.id cities.name state.name
+SELECT cities.id, cities.name, state.name
 From cities
 INNER JOIN states ON cities.state_id = state.id
 ORDER BY cities.id ASC;
