148b979c59aed907d501c655cc86b498cebb7612
Simn-popo <paulin.simon@live.fr>
2025-06-25 10:40:02 +0200

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
--- create table and give perm to user
+-- Create a table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    states_id INT NOT NULL,
-    FOREIGN KEY (states_id) REFERENCES states(id),
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
