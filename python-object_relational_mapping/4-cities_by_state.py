3886bad711e0e43cdb3e987fcb63e9862c4c4592
Simn-popo <paulin.simon@live.fr>
2025-06-27 15:59:58 +0200

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """
-list cities
+Script that lists all cities from the database hbtn_0e_4_usa
+with their associated state names, ordered by cities.id.
 """
+
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    dbname = sys.argv[3]
-    state_name = sys.argv[4]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=dbname
+        db=database
     )
 
     cur = db.cursor()
-
     cur.execute("""
         SELECT cities.id, cities.name, states.name
         FROM cities
