697ede34ccc10c31f44117a87827aadcd4611ec6
Simn-popo <paulin.simon@live.fr>
2025-06-27 16:42:31 +0200

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """
-intro to alchimy class state
+..
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    Define class states.
+    """
     __tablename__ = 'states'
+
     id = Column(Integer, auto_increment=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
