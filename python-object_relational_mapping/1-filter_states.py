07ed7cef57d9b20a440a7b0dc230f447c1e8e707
Simn-popo <paulin.simon@live.fr>
2025-06-27 14:51:09 +0200

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    data_base = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        data_base=database
+    )
+    cur = data_base.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%N' ORDER BY id ASC")
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    data_base.close()
