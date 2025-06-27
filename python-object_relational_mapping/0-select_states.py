8d3bc209bfd4958a294913c6b5bf48055bcfe7ca
Simn-popo <paulin.simon@live.fr>
2025-06-27 14:46:34 +0200

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""List a database"""
-
-import sys
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
+import sys
 
-
-def main():
-    """List a database"""
-
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=db_name
+        db=database
     )
-
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     for row in cur.fetchall():
@@ -29,7 +25,3 @@ def main():
 
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
