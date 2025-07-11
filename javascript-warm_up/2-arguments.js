3f3368a5701ec441413043e94070040958eeb08b
Simon-Paulin <paulin.simon@live.fr>
2025-07-11 12:05:49 +0200

    fix output

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 const CountArg = process.argv.slice(2);
-if (CountArg.length === 0)
-    {
+if (CountArg.length === 0){
         console.log("No argument");
-} 
-else if (CountArg.length === 1)
-    {
+}else if (CountArg.length === 1){
         console.log("Argument found");
-} 
-else {
-    console.log("Argument found");
+}else {
+    console.log("Arguments found");
 }
