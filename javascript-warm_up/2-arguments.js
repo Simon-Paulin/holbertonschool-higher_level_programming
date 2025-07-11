a7a132f90ca913cec9fb893066e0201af498db48
Simon-Paulin <paulin.simon@live.fr>
2025-07-11 12:08:39 +0200

    fix semi

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const CountArg = process.argv.slice(2);
-if (CountArg.length === 0){
-        console.log("No argument");
-}else if (CountArg.length === 1){
-        console.log("Argument found");
-}else {
-    console.log("Arguments found");
+
+if (CountArg.length === 0) {
+  console.log("No argument");
+} else if (CountArg.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
