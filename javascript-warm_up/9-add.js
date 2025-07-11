bafcd69a289442e5e0bae5b78e01829bb4ab528a
Simon-Paulin <paulin.simon@live.fr>
2025-07-11 15:07:23 +0200

    fix

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function add (a, b) {
-  console.log(a +b);
+  console.log(a + b);
 }
 
-const num1 = Number(success.argv[2]);
-const num2 = Number(success.argv [3]);
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv [3]);
 
 add(num1, num2);
