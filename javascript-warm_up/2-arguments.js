43432848cb8aa09246d777a9060dfe04ec03a44d
Simon-Paulin <paulin.simon@live.fr>
2025-07-11 12:03:02 +0200

    task2: count args

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const CountArg = process.argv.slice(2);
+if (CountArg.lenght === 0)
+    {
+        console.log("No argument");
+} 
+else if (CountArg === 1)
+    {
+        console.log("Argument found");
+} 
+else {
+    console.log("Argument found");
+}
