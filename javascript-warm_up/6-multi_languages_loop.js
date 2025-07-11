2c8c2518a7a7afbc87c1939bba2b4b0a248e4df1
Simon-Paulin <paulin.simon@live.fr>
2025-07-11 14:43:05 +0200

    fix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const a = ['C is fun\nPython is cool\nJavaScript is amazing']
+const a = ['C is fun\nPython is cool\nJavaScript is amazing'];
 let i = 0;
 
 for (i = 0; a [i]; i++) {
